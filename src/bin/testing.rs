28271d298b1020c69170964afdde0405f73ad966
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2025-08-21 14:02:22 +1200

    Shortened `main`

--- src/bin/testing.rs
+++ src/bin/testing.rs
@@ -3,20 +3,14 @@ use langite::{parsing::parse_file, resolving::resolve_program};
 fn main() {
     let filepath = "test.lang";
     let source = std::fs::read_to_string(filepath).unwrap();
-    let items = match parse_file(filepath.into(), &source) {
-        Ok(items) => items,
-        Err(error) => {
-            eprintln!("{error}");
-            std::process::exit(1)
-        }
-    };
-    let (resolved, names) = match resolve_program(&items) {
-        Ok(output) => output,
-        Err(error) => {
-            eprintln!("{error}");
-            std::process::exit(1)
-        }
-    };
+    let items = parse_file(filepath.into(), &source).unwrap_or_else(|error| {
+        eprintln!("{error}");
+        std::process::exit(1)
+    });
+    let (resolved, names) = resolve_program(&items).unwrap_or_else(|error| {
+        eprintln!("{error}");
+        std::process::exit(1)
+    });
     drop(items);
     println!("{:#?}", resolved.types);
     println!("{:#?}", resolved.functions);
